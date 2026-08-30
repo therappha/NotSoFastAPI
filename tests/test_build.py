43c66e8a258e31e91bc31867fa19070798e44b42
therappha <therappha@gmail.com>
2026-08-30 02:07:20 +0100

    feat(build): #2 added basic setup files .github and pyproject pytest

---
+++ tests/test_build.py
@@ -0,0 +1,8 @@
+"""File to test the if the build requirements are work"""
+
+
+class TestPyTestSetup:
+    """Confirm build with pytest is working"""
+
+    def test_pytest_is_active(self):
+        assert 1 == 1
