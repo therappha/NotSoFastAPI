2efe77ff71ee44d3b8956ead7ccb034594f01523
therappha <therappha@gmail.com>
2026-09-04 02:33:50 +0100

    feat(http/responses): #10 added simple response encoder

--- src/notsofastapi/http/response.py
+++ src/notsofastapi/http/response.py
@@ -6,10 +6,10 @@ from notsofastapi.http.typing import HttpStatus
 class HttpResponse:
     """Represents an http response"""
 
-    def __init__(self, body: str = None, headers: dict = None, status=200):
+    def __init__(self, body: str = None, headers: dict = None, status: int = 200):
 
         self.version: str = "HTTP/1.1"
-        self.status: HttpStatus = status
+        self.status: HttpStatus = HttpStatus(status)
         self.body: bytes = None
         self.headers: dict = {}
 
@@ -20,3 +20,27 @@ class HttpResponse:
         if headers:
             for key, value in headers.items():
                 self.headers[key] = value
+
+    def encode_headers(self) -> bytes:
+        header_bytes = b""
+
+        for key, value in self.headers.items():
+            header_bytes += f"{key}: {value}\r\n".encode("utf-8")
+
+        return header_bytes
+
+    def encode_response(self) -> bytes:
+        response = b""
+
+        http_line = f"{self.version} {self.status.value} {self.status.name}\r\n".encode(
+            "utf-8"
+        )
+        header_bytes = self.encode_headers()
+
+        response += http_line
+        if header_bytes != b"":
+            response += header_bytes
+        response += "\r\n".encode("utf-8")
+        if self.body is not None:
+            response += self.body
+        return response
